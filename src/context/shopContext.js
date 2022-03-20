94db8b00d223406b9f7e978d673981093a93479f
kenbom <tsutomu.kimura@nifty.ne.jp>
2022-03-20 08:20:26 +0000

    start project

---
+++ src/context/shopContext.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Client from 'shopify-buy'
+
+export class shopContext extends Component {
+  render() {
+    return (
+      <div>shopContext</div>
+    )
+  }
+}
+
+export default shopContext
